34ab8c3ac9b7d87069670cf828f23dfe02f33a0f
Adam Bair <adambair@gmail.com>
2009-07-16 10:59:36 -0400

    Tests can now be run without a host Rails application (standalone)

--- test/test_harness.rb
+++ test/test_harness.rb
@@ -2,17 +2,21 @@ require 'test/unit'
 require 'yaml'
 
 begin
-  require File.dirname(__FILE__) + '/../../../../config/boot'
-  require 'active_record'
+  require File.dirname(__FILE__) + '/../../../../config/environment'
 rescue LoadError
   require 'rubygems'
-  require_gem 'activerecord'
+  gem 'activerecord'
+  gem 'actionpack'
+  require 'active_record'
+  require 'action_controller'
 end
 
 require File.dirname(__FILE__) + '/../lib/profanity_filter'
 
-config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
-ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite3'])
+ActiveRecord::Base.configurations = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
+ActiveRecord::Base.establish_connection('test')
 load(File.dirname(__FILE__) + "/schema.rb")
+
 ActiveRecord::Base.send(:include, ProfanityFilter)
+

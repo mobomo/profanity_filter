d7a023a7fafaecb668cd515b73a1c4e762705588
Neil Ang <neil@neilang.com>
2009-08-05 13:25:04 +1000

    Will now filter punctionation delimited profanity. Added more word replacement options.

--- test/benign_filter_test.rb
+++ test/benign_filter_test.rb
@@ -12,11 +12,23 @@ module DictionaryPostHelper
   end
 end
 
+module VowelsPostHelper
+  class Post < ActiveRecord::Base
+    profanity_filter :title, :body, :method => 'vowels'
+  end
+end
+
+module HollowPostHelper
+  class Post < ActiveRecord::Base
+    profanity_filter :title, :body, :method => 'hollow'
+  end
+end
+
 class BasicProfanityFilterTest < Test::Unit::TestCase
   include BasicPostHelper
 
   def profane_post(opts={})
-    Post.new({:title => 'A Fucking Title', :body => "This is some shitty post by a fucking user"}.merge(opts))
+    Post.new({:title => 'A Fucking Title', :body => "This is some f-u-c-k-i-n-g shitty-post by a fucking user"}.merge(opts))
   end
   
   def test_it_should_filter_specified_fields
@@ -25,9 +37,9 @@ class BasicProfanityFilterTest < Test::Unit::TestCase
     assert_equal 'A @#$% Title', p.title
     assert_equal 'A @#$% Title', p.title_clean
     assert_equal 'A Fucking Title', p.title_original
-    assert_equal 'This is some @#$% post by a @#$% user', p.body
-    assert_equal 'This is some @#$% post by a @#$% user', p.body_clean
-    assert_equal 'This is some shitty post by a fucking user', p.body_original
+    assert_equal 'This is some @#$% @#$%-post by a @#$% user', p.body
+    assert_equal 'This is some @#$% @#$%-post by a @#$% user', p.body_clean
+    assert_equal 'This is some f-u-c-k-i-n-g shitty-post by a fucking user', p.body_original
   end
   
   def test_it_should_handle_nil_fields_bug_9
@@ -49,7 +61,7 @@ class DictionaryProfanityFilterTest < Test::Unit::TestCase
   include DictionaryPostHelper
   
   def profane_post(opts={})
-    Post.new({:title => 'A Fucking Title', :body => "This is some shitty post by a fucking user"}.merge(opts))
+    Post.new({:title => 'A Fucking Title', :body => "This is some f-u-c-k-i-n-g shitty-post by a fucking user"}.merge(opts))
   end
   
   def test_it_should_filter_specified_fields
@@ -58,9 +70,75 @@ class DictionaryProfanityFilterTest < Test::Unit::TestCase
     assert_equal 'A f*ck*ng Title', p.title
     assert_equal 'A f*ck*ng Title', p.title_clean
     assert_equal 'A Fucking Title', p.title_original
-    assert_equal 'This is some sh*tty post by a f*ck*ng user', p.body
-    assert_equal 'This is some sh*tty post by a f*ck*ng user', p.body_clean
-    assert_equal 'This is some shitty post by a fucking user', p.body_original
+    assert_equal 'This is some f*ck*ng sh*tty-post by a f*ck*ng user', p.body
+    assert_equal 'This is some f*ck*ng sh*tty-post by a f*ck*ng user', p.body_clean
+    assert_equal 'This is some f-u-c-k-i-n-g shitty-post by a fucking user', p.body_original
+  end
+  
+  def test_it_should_handle_nil_fields_bug_9
+    p = Post.new({:title => nil, :body => nil})
+    p.save
+    assert_equal nil, p.title
+    assert_equal nil, p.body
+  end
+  
+  def test_it_should_handle_blank_fields_bug_9
+    p = Post.new({:title => "", :body => ""})
+    p.save
+    assert_equal "", p.title
+    assert_equal "", p.body
+  end
+end
+
+class VowelsProfanityFilterTest < Test::Unit::TestCase
+  include VowelsPostHelper
+  
+  def profane_post(opts={})
+    Post.new({:title => 'A Fucking Title', :body => "This is some f-u-c-k-i-n-g shitty-post by a fucking user"}.merge(opts))
+  end
+  
+  def test_it_should_filter_specified_fields
+    p = profane_post
+    p.save
+    assert_equal 'A F*ck*ng Title', p.title
+    assert_equal 'A F*ck*ng Title', p.title_clean
+    assert_equal 'A Fucking Title', p.title_original
+    assert_equal 'This is some f*ck*ng sh*tty-post by a f*ck*ng user', p.body
+    assert_equal 'This is some f*ck*ng sh*tty-post by a f*ck*ng user', p.body_clean
+    assert_equal 'This is some f-u-c-k-i-n-g shitty-post by a fucking user', p.body_original
+  end
+  
+  def test_it_should_handle_nil_fields_bug_9
+    p = Post.new({:title => nil, :body => nil})
+    p.save
+    assert_equal nil, p.title
+    assert_equal nil, p.body
+  end
+  
+  def test_it_should_handle_blank_fields_bug_9
+    p = Post.new({:title => "", :body => ""})
+    p.save
+    assert_equal "", p.title
+    assert_equal "", p.body
+  end
+end
+
+class HollowProfanityFilterTest < Test::Unit::TestCase
+  include HollowPostHelper
+  
+  def profane_post(opts={})
+    Post.new({:title => 'A Fucking Title', :body => "This is some f-u-c-k-i-n-g shitty-post by a fucking user"}.merge(opts))
+  end
+  
+  def test_it_should_filter_specified_fields
+    p = profane_post
+    p.save
+    assert_equal 'A F*****g Title', p.title
+    assert_equal 'A F*****g Title', p.title_clean
+    assert_equal 'A Fucking Title', p.title_original
+    assert_equal 'This is some f*****g s****y-post by a f*****g user', p.body
+    assert_equal 'This is some f*****g s****y-post by a f*****g user', p.body_clean
+    assert_equal 'This is some f-u-c-k-i-n-g shitty-post by a fucking user', p.body_original
   end
   
   def test_it_should_handle_nil_fields_bug_9
--- test/profanity_filter_test.rb
+++ test/profanity_filter_test.rb
@@ -10,8 +10,27 @@ class BasicProfanityFilterTest < Test::Unit::TestCase
     assert_equal 'happy', ProfanityFilter::Base.clean('happy')
   end
   
+  def test_basic_profanity_filter_does_not_modify_whitespace
+    assert_equal 'hello  world', ProfanityFilter::Base.clean('hello  world')
+    assert_equal "hello \t world", ProfanityFilter::Base.clean("hello \t world")
+    assert_equal "hello \n world", ProfanityFilter::Base.clean("hello \n world")
+  end
+  
+  def test_basic_profanity_filter_does_not_modify_special_characters
+    assert_equal 'happy  @#$%', ProfanityFilter::Base.clean('happy  fuck')
+    assert_equal 'happy\'s', ProfanityFilter::Base.clean('happy\'s')
+    assert_equal '@#$%\'s', ProfanityFilter::Base.clean('fuck\'s')
+    assert_equal '@#$%?!', ProfanityFilter::Base.clean('fuck?!')
+  end
+  
   def test_basic_profanity_filter_replaces_profane_words
     assert_equal '@#$%', ProfanityFilter::Base.clean('fuck')
+  end
+  
+  def test_basic_profanity_filter_replaces_punctuation_spaced_profane_words
+    assert_equal '@#$%', ProfanityFilter::Base.clean('f-u-c-k')
+    assert_equal '@#$%', ProfanityFilter::Base.clean('f.u.c.k')
+    assert_equal 'happy-@#$%', ProfanityFilter::Base.clean('happy-fuck')
   end
 end
 
@@ -20,7 +39,87 @@ class DictionaryProfanityFilterTest < Test::Unit::TestCase
     assert_equal 'happy', ProfanityFilter::Base.clean('happy', 'dictionary')
   end
   
+  def test_dictionary_profanity_filter_does_not_modify_whitespace
+    assert_equal 'hello  world', ProfanityFilter::Base.clean('hello  world', 'dictionary')
+    assert_equal "hello \t world", ProfanityFilter::Base.clean("hello \t world", 'dictionary')
+    assert_equal "hello \n world", ProfanityFilter::Base.clean("hello \n world", 'dictionary')
+  end
+  
+  def test_dictionary_profanity_filter_does_not_modify_special_characters
+    assert_equal 'happy  f*ck', ProfanityFilter::Base.clean('happy  fuck', 'dictionary')
+    assert_equal 'happy\'s', ProfanityFilter::Base.clean('happy\'s', 'dictionary')
+    assert_equal 'f*ck\'s', ProfanityFilter::Base.clean('fuck\'s', 'dictionary')
+    assert_equal 'f*ck?!', ProfanityFilter::Base.clean('fuck?!', 'dictionary')
+  end 
+  
   def test_dictionary_profanity_filter_replaces_profane_words
     assert_equal 'f*ck', ProfanityFilter::Base.clean('fuck', 'dictionary')
+  end
+  
+  def test_dictionary_profanity_filter_replaces_punctuation_spaced_profane_words
+    assert_equal 'f*ck', ProfanityFilter::Base.clean('f-u-c-k', 'dictionary')
+    assert_equal 'f*ck', ProfanityFilter::Base.clean('f.u.c.k', 'dictionary')
+    assert_equal 'happy-f*ck', ProfanityFilter::Base.clean('happy-fuck', 'dictionary')
+  end
+end
+
+
+class VowelsProfanityFilterTest < Test::Unit::TestCase
+  def test_vowels_profanity_filter_does_not_modify_clean_words
+    assert_equal 'happy', ProfanityFilter::Base.clean('happy', 'vowels')
+  end
+  
+  def test_vowels_profanity_filter_does_not_modify_whitespace
+    assert_equal 'hello  world', ProfanityFilter::Base.clean('hello  world', 'vowels')
+    assert_equal "hello \t world", ProfanityFilter::Base.clean("hello \t world", 'vowels')
+    assert_equal "hello \n world", ProfanityFilter::Base.clean("hello \n world", 'vowels')
+  end
+  
+  def test_vowels_profanity_filter_does_not_modify_special_characters
+    assert_equal 'happy  f*ck', ProfanityFilter::Base.clean('happy  fuck', 'vowels')
+    assert_equal 'happy\'s', ProfanityFilter::Base.clean('happy\'s', 'vowels')
+    assert_equal 'f*ck\'s', ProfanityFilter::Base.clean('fuck\'s', 'vowels')
+    assert_equal 'f*ck?!', ProfanityFilter::Base.clean('fuck?!', 'vowels')
+  end
+  
+  def test_vowels_profanity_filter_replaces_profane_words
+    assert_equal 'f*ck', ProfanityFilter::Base.clean('fuck', 'vowels')
+    assert_equal 'F*CK', ProfanityFilter::Base.clean('FUCK', 'vowels')
+  end
+  
+  def test_vowels_profanity_filter_replaces_punctuation_spaced_profane_words
+    assert_equal 'f*ck', ProfanityFilter::Base.clean('f-u-c-k', 'vowels')
+    assert_equal 'f*ck', ProfanityFilter::Base.clean('f.u.c.k', 'vowels')
+    assert_equal 'happy-f*ck', ProfanityFilter::Base.clean('happy-fuck', 'vowels')
+  end
+end
+
+class HollowProfanityFilterTest < Test::Unit::TestCase
+  def test_hollow_profanity_filter_does_not_modify_clean_words
+    assert_equal 'happy', ProfanityFilter::Base.clean('happy', 'hollow')
+  end
+  
+  def test_hollow_profanity_filter_does_not_modify_whitespace
+    assert_equal 'hello  world', ProfanityFilter::Base.clean('hello  world', 'hollow')
+    assert_equal "hello \t world", ProfanityFilter::Base.clean("hello \t world", 'hollow')
+    assert_equal "hello \n world", ProfanityFilter::Base.clean("hello \n world", 'hollow')
+  end
+  
+  def test_hollow_profanity_filter_does_not_modify_special_characters
+    assert_equal 'happy  f**k', ProfanityFilter::Base.clean('happy  fuck', 'hollow')
+    assert_equal 'happy\'s', ProfanityFilter::Base.clean('happy\'s', 'hollow')
+    assert_equal 'f**k\'s', ProfanityFilter::Base.clean('fuck\'s', 'hollow')
+    assert_equal 'f**k?!', ProfanityFilter::Base.clean('fuck?!', 'hollow')
+  end
+  
+  def test_hollow_profanity_filter_replaces_profane_words
+    assert_equal 'f**k', ProfanityFilter::Base.clean('fuck', 'hollow')
+    assert_equal 'F**K', ProfanityFilter::Base.clean('FUCK', 'hollow')
+  end
+  
+  def test_hollow_profanity_filter_replaces_punctuation_spaced_profane_words
+    assert_equal 'f**k', ProfanityFilter::Base.clean('f-u-c-k', 'hollow')
+    assert_equal 'f**k', ProfanityFilter::Base.clean('f.u.c.k', 'hollow')
+    assert_equal 'happy-f**k', ProfanityFilter::Base.clean('happy-fuck', 'hollow')
   end
 end
